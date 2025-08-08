924a24b2c5f3115cd2ea5fa0af3eac0482f95ab1
Ekambaram Gowthami <ekambaramgowthami759@gmail.com>
2025-08-08 13:11:28 +0530

    Update SearchElement.tsx

--- frontend/src/components/SearchElement.tsx
+++ frontend/src/components/SearchElement.tsx
@@ -55,7 +55,7 @@ export const SearchElement = () => {
       const techs = extractOf([combinedPrompt]);
      const language=getPrimaryLanguage(techs);
 
-      const res = await axios.post("http://localhost:3000/template", { prompt: combinedPrompt });
+      const res = await axios.post("https://bolt-backend-d3qn.onrender.com/template", { prompt: combinedPrompt });
       const result = res.data;
       setData(result);
 
