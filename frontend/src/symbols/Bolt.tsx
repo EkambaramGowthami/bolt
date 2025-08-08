fa8b4f20f5c799ef552d130792493d894f79beb2
Ekambaram Gowthami <ekambaramgowthami759@gmail.com>
2025-08-08 13:28:25 +0530

    Update Bolt.tsx

--- frontend/src/symbols/Bolt.tsx
+++ frontend/src/symbols/Bolt.tsx
@@ -1,23 +1,24 @@
 export const Bolt = () => {
   return (
-    <svg
-      version="1.1"
-      id="Layer_1"
-      xmlns="http://www.w3.org/2000/svg"
-      xmlnsXlink="http://www.w3.org/1999/xlink"
-      x="0px"
-      y="0px"
-      viewBox="0 0 81.83 122.88"
-      style={{ enableBackground: "new 0 0 81.83 122.88" }}
-      xmlSpace="preserve"
-    >
-      <g>
-        <polygon
-          fillRule="evenodd"
-          clipRule="evenodd"
-          points="33.05,63.12 0,60.01 27.4,0 64.86,0 43.62,34.43 81.83,38.66 11.69,122.88 33.05,63.12"
-        />
-      </g>
-    </svg>
+    // <svg
+    //   version="1.1"
+    //   id="Layer_1"
+    //   xmlns="http://www.w3.org/2000/svg"
+    //   xmlnsXlink="http://www.w3.org/1999/xlink"
+    //   x="0px"
+    //   y="0px"
+    //   viewBox="0 0 81.83 122.88"
+    //   style={{ enableBackground: "new 0 0 81.83 122.88" }}
+    //   xmlSpace="preserve"
+    // >
+    //   <g>
+    //     <polygon
+    //       fillRule="evenodd"
+    //       clipRule="evenodd"
+    //       points="33.05,63.12 0,60.01 27.4,0 64.86,0 43.62,34.43 81.83,38.66 11.69,122.88 33.05,63.12"
+    //     />
+    //   </g>
+    // </svg> 
+    <div>hi there</div>
   );
 };
