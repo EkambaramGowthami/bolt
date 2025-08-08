f87df11cb7fe7cfb494c19d8901d7a21913c4882
Ekambaram Gowthami <ekambaramgowthami759@gmail.com>
2025-08-08 13:13:54 +0530

    Update signup.tsx

--- frontend/src/smallCom/signup.tsx
+++ frontend/src/smallCom/signup.tsx
@@ -4,7 +4,7 @@ import { useNavigate } from "react-router-dom";
 
 export const Signup = () =>{
   const GOOGLE_CLIENT_ID=process.env.GOOGLE_CLIENT_ID!
-  const REDIRECT_URI = "http://localhost:3000/api/auth/google/callback";
+  const REDIRECT_URI = "https://bolt-backend-d3qn.onrender.com/api/auth/google/callback";
   const handleSigninWithGoogle = ()=>{
     const scope = encodeURIComponent("email profile");
     const url = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${GOOGLE_CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code&scope=${scope}`;
