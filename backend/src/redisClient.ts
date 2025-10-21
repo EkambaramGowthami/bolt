b48f2dd14f7616e712dbfd680c5b1aaaa591d5c7
Gowthami <ekambaramgowthami759@gmail.com>
2025-10-21 21:44:51 +0530

    Save local changes before pull

--- backend/src/redisClient.ts
+++ backend/src/redisClient.ts
@@ -9,20 +9,3 @@ redisClient.on("connect",()=>console.log("Redis Client Connected"));
     await redisClient.connect();
 })();
 export default redisClient;
-// testRedis.ts
-// import { createClient } from "redis";
-// import { config } from "dotenv";
-// config();
-
-// const redisClient = createClient({ url: process.env.REDIS_URL });
-// redisClient.on("connect", () => console.log("✅ Connected to Redis Cloud"));
-// redisClient.on("error", (err) => console.error("❌ Redis Error:", err));
-
-// (async () => {
-//   await redisClient.connect();
-//   await redisClient.set("testkey", "HelloRedis");
-//   const val = await redisClient.get("testkey");
-//   console.log("Fetched:", val);
-//   await redisClient.quit();
-// })();
-// export default redisClient;
